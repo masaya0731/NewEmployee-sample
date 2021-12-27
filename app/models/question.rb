df8155d4b1397fee6bdd6df7fe75398ef1747aed
masaya0731 <masaya0806543@gmail.com>
2021-12-27 08:37:56 +0000

    [Add] controller

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,6 @@
 class Question < ApplicationRecord
+
+  belongs_to :customer
+  belongs_to :genre
+  has_many :answers, dependent: :destroy
 end
---
+++ test/controllers/public/answer_notifications_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Public::AnswerNotificationsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
