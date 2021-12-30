b6168abf4f9f7907ea9678a71db1919ba5650d41
masaya0731 <masaya0806543@gmail.com>
2021-12-30 13:50:57 +0000

    active_admin test

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,13 @@
 class Public::HomesController < ApplicationController
-  
+
   def top
+    @author = Author.all
   end
-  
+
   def about
+  end
+
+  def author_params
+    params.require(:author).permit(:name)
   end
 end
